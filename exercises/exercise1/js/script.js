8bd84ba2f8ca462a2cf231711f845f3d6d7bea3d
fgorgacheva <fgorgacheva@gmail.com>
2020-09-23 14:52:29 -0700

    added size changing properties

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -60,26 +60,35 @@ function draw() {
   background(255, 201, 201);
   noStroke();
 
-  let my = map(mouseY,0,height,0,255);
-  let mx = map(mouseX,0,width,0,255);
+  let mmy = map(mouseY,0,height,0,255);
+  let mmx = map(mouseX,0,width,0,255);
+
+  let cmx = constrain(mouseX, 0, width);
+  cmx = constrain(cmx, 0, 600);
+  let cmy = constrain(mouseY, 0, height);
+  cmy = constrain(cmy, 0, 600);
+
 
   // bol.x = constrain(bol.x, 0, width);
-  // bol.fill = map(bol.x, 0, width, 0, 255);
 
   //** CIRCLE ===========================================================================================
 
   //when bol hits any side of the canvas, reverse direction
-  bol.speedX *= ((bol.x + bol.size/2 >= width || bol.x - bol.size/2 <= 0) ? -1 : 1);//+ bol.size/2
+  bol.speedX *= ((bol.x + bol.size/2 >= width  || bol.x - bol.size/2 <= 0) ? -1 : 1);//+ bol.size/2
   bol.speedY *= ((bol.y + bol.size/2 >= height || bol.y - bol.size/2 <= 0) ? -1 : 1);//- bol.size/2
 
   bol.x = bol.x + bol.speedX;
   bol.y = bol.y + bol.speedY;
 
   //make circle color change randomly based on mouse position on the canvas
-  fill(mx, my, 255);
+  fill(mmx, mmy, 255);
 
   //make the circle size change based on mouse position within the canvas
-  //bol.size = constrain(mouseX,0,500);
+  if (!(bol.x + cmx/2 > width) && !(bol.y + cmx/2 > height) &&
+      !(bol.x - cmx/2 < 0)     && !(bol.y - cmx/2 < 0) ){
+
+    bol.size = cmx;
+  }
 
   //draw the circle
   circle(bol.x, bol.y, bol.size);
@@ -89,32 +98,47 @@ function draw() {
   //** SQUARE ===========================================================================================
 
   //when skwar hits any side of the canvas, reverse direction
-  skwar.speedX *= ((skwar.x + skwar.size > width || skwar.x < 0) ? -1 : 1);
+  skwar.speedX *= ((skwar.x + skwar.size > width  || skwar.x < 0) ? -1 : 1);
   skwar.speedY *= ((skwar.y + skwar.size > height || skwar.y < 0) ? -1 : 1);
 
   skwar.x = skwar.x + skwar.speedX;
   skwar.y = skwar.y + skwar.speedY;
 
-  fill(my, 255, mx);
+  fill(mmy, 255, mmx);
 
-  //skwar.size = constrain(mouseY,0,500)
+  //make the circle size change based on mouse position within the canvas
+  if (!(skwar.x + cmy > width) && !(skwar.y + cmy > height) /*&&
+      !(skwar.x - cmx < 0)     && !(skwar.y - cmx < 0)*/ ){
 
+    skwar.size = cmy;
+  }
+
+  //draw the square
   square(skwar.x, skwar.y, skwar.size);
+
 
 
   //** ELLIPSE ===========================================================================================
 
   //when ellps hits any side of the canvas, reverse direction
-  ellps.speedX *= ((ellps.x + ellps.size > width || ellps.x < 0) ? -1 : 1);
-  ellps.speedY *= ((ellps.y + ellps.size > height || ellps.y < 0) ? -1 : 1);
+
 
   ellps.x = ellps.x + ellps.speedX;
   ellps.y = ellps.y + ellps.speedY;
 
-  fill(255, my, mx);
+  fill(255, mmy, mmx);
 
-  //skwar.size = constrain(mouseY,0,500)
+  if (!(ellps.x + cmx/2 > width) && !(ellps.y + cmy/2 > height) &&
+      !(ellps.x - cmx/2 < 0)     && !(ellps.y - cmy/2 < 0) ){
 
+        ellps.w = cmx;
+        ellps.h = cmy;
+      }
+
+  ellps.speedX *= ((ellps.x + ellps.w/2 > width  || ellps.x - ellps.w/2< 0) ? -1 : 1);
+  ellps.speedY *= ((ellps.y + ellps.h/2 > height || ellps.y - ellps.h/2< 0) ? -1 : 1);
+
+  //draw the ellipse
   ellipse(ellps.x, ellps.y, ellps.w, ellps.h);
 
 
