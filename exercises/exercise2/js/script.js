4b0eccba49c5f168828d394e851a18406bc2d7f4
fgorgacheva <fgorgacheva@gmail.com>
2020-09-30 19:14:13 -0700

    change virus behavior and add images

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -7,11 +7,15 @@ Here is a description of this template p5 project.
 
 let covid19 = {
   x: 0,
-  y: 250,
+  y: 0,
   size: 100,
-  vx: 0,
-  vy: 0,
-  speed: 5,
+  vx: 2,
+  vy: 3,
+  distanceX: 0,
+  distanceY: 0,
+  targetX: 0,
+  targetY: 0,
+  ease: 0.02
 }
 
 let user = {
@@ -23,16 +27,38 @@ let user = {
   speed: 5,
 }
 
+let house = {
+  x: 50,
+  y: 0,
+  size: 400
+}
+
+let bgImg;
+let covidImg;
+let houseImg;
+let endImg;
+
+function preload() {
+  bgImg = loadImage('assets/images/background.png');
+  covidImg = loadImage('assets/images/covid19s.png');
+  houseImg = loadImage('assets/images/house.png');
+  endImg = loadImage('assets/images/stop.png');
+}
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  covid19.y = random(0, height);
-  covid19.vx = covid19.speed;
-
+  noStroke();
   //noCursor();
+
+  covid19.x = random(50, windowWidth-50);
+  covid19.y = random(50, windowHeight-50);
+
+  house.y = windowHeight-450;
+
+  image(bgImg, 0, 0);
 
 }
 
@@ -40,19 +66,25 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-  background(0);
+  background(bgImg);
 
-  covid19.x = covid19.x + covid19.vx;
-  covid19.y = covid19.y + covid19.vy;
+  // covid19 movement -> it follows the user
+  covid19.targetX = mouseX;
+  covid19.distanceX = covid19.targetX - covid19.x;
+  covid19.x += covid19.distanceX * covid19.ease;
+
+  covid19.targetY = mouseY;
+  covid19.distanceY = covid19.targetY - covid19.y;
+  covid19.y += covid19.distanceY * covid19.ease;
 
   if(covid19.x > width){
-    covid19.x = 0;
+    covid19.x = random(0, height);
     covid19.y = random(0, height);
   }
 
   //place house
-  fill(255,0,255);
-  square(0, windowHeight-400, 400);
+  texture(houseImg);
+  square(house.x, house.y, house.size);
 
   //user movement
   user.x = mouseX;
@@ -60,10 +92,16 @@ function draw() {
 
   //check if user got covid19
   let distance = dist(user.x, user.y, covid19.x, covid19.y);
-
   if(distance < user.size/2 + covid19.size/2){
     noLoop();
   }
+
+  //if covid touches house, bounce away
+  covid19.vx *= ((covid19.x + covid19.size/2 >= house.x) ? -1 : 1);
+  covid19.vy *= ((covid19.y - covid19.size/2 <= house.y) ? -1 : 1);
+
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
 
   //display covid 19 circle
   fill(255,0,0);
