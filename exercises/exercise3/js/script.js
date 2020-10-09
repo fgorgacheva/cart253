77fa56e2dae9c67c730e87559e39b2f903f6e055
fgorgacheva <fgorgacheva@gmail.com>
2020-10-08 21:25:41 -0700

    add sounds and title page

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -43,6 +43,8 @@ let deathImg;
 let arkhamImg;
 
 let jokerSound;
+let happyEndSound;
+let sadEndSound;
 
 let gameOver;
 
@@ -58,6 +60,8 @@ function preload() {
 
   soundFormats('mp3');
   jokerSound = loadSound('assets/sounds/joker easter egg.mp3');
+  happyEndSound = loadSound('assets/sounds/batman and robin.mp3');
+  sadEndSound = loadSound('assets/sounds/separate ways.mp3');
 }
 
 // setup()
@@ -84,9 +88,60 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  title();
 
+  function mouseClicked(){
+    loadGame();
+  }
+
+}
+
+//defines batmans behavior
+function batmanDraw(){
+batman.tx = batman.tx + 0.025;
+batman.ty = batman.ty + 0.025;
+
+let noiseX = noise(batman.tx);
+let noiseY = noise(batman.ty);
+
+batman.vx = map(noiseX, 0, 1, - batman.speed, batman.speed);
+batman.vy = map(noiseY, 0, 1, - batman.speed, batman.speed);
+
+batman.x = batman.x + batman.vx;
+batman.y = batman.y + batman.vy;
+
+imageMode(CENTER);
+image(batmanImg, batman.x, batman.y, batman.size, batman.size);
+imageMode(CORNER);
+}
+
+
+//display a title screen
+function title(){
+  background('black');
+
+  fill('yellow');
+  textSize(100);
+  textAlign(CENTER);
+  text('The Batman and Robin Matching Game', windowWidth/2, 150);
+
+  fill('white');
+  textSize(20);
+  textAlign(LEFT);
+  text('- CONTEXT: Jason Todd was one of the famous Robins. He was kidnapped and killed by the Joker in a warehouse explosion.', 250, 250);
+  text('- RULES: Make Bruce and Jason\'s paths cross for them to become Batman anad Robin. If either of them leave the screen, they go their spearate ways.', 250, 350);
+  text('Place Jason over the warehouse for a guest appearance', 340, 400);
+  text('**DISCLAIMER: There are sound effects and they may be very loud. Please lower your volume before playing.', 250, 500);
+
+  fill('red');
+  textSize(50);
+  textAlign(CENTER);
+  text('Click Anywhere to Play!', windowWidth/2, 700);
+}
+
+//load game
+function loadGame(){
   background(arkhamImg);
-
 
   //check if jason met bruce
   let distance = dist(robin.x, robin.y, batman.x, batman.y);
@@ -125,35 +180,9 @@ function draw() {
   }
 }
 
-
-//defines batmans behavior
-function batmanDraw(){
-batman.tx = batman.tx + 0.025;
-batman.ty = batman.ty + 0.025;
-
-let noiseX = noise(batman.tx);
-let noiseY = noise(batman.ty);
-
-batman.vx = map(noiseX, 0, 1, - batman.speed, batman.speed);
-batman.vy = map(noiseY, 0, 1, - batman.speed, batman.speed);
-
-batman.x = batman.x + batman.vx;
-batman.y = batman.y + batman.vy;
-
-imageMode(CENTER);
-image(batmanImg, batman.x, batman.y, batman.size, batman.size);
-imageMode(CORNER);
-}
-
-
-//displlay a title screen
-function title(){
-
-}
-
 //end the program and display happy outcome image
 function happiness() {
-  //make bruce and jason disappear
+  happyEndSound.play();
   gameOver = true;
   imageMode(CENTER);
   image(happyEndImg, windowWidth/2, windowHeight/2, 500, 700);
@@ -163,7 +192,7 @@ function happiness() {
 
 //disaply sad outcome
 function sadness() {
-  //make bruce and jason disappear
+  sadEndSound.play();
   gameOver = true;
   imageMode(CENTER);
   image(sadEndImg, windowWidth/2, windowHeight-400, 800,800);
