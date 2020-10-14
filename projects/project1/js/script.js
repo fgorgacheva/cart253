8bc20402ca6baf34459db3f78db3edbce9de2da8
fgorgacheva <fgorgacheva@gmail.com>
2020-10-13 17:57:15 -0700

    P1: make overall skeleton

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,9 +1,61 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Project 1: Space travel and discovery simulation
 
-Here is a description of this template p5 project.
+You are in space, you control a spaceship.
+You can explore pace to learn more about various cosmic objects placed around.
+To learn about an object, press and hold the object to make an info tab appear.
+Alien ship will float around at random moments, avoid it to avoid trouble.
+If you come in contact with the aliens, they will give you a choice
+  1) you can apologize and continue your journey
+  2) you can be aggressive and they will kill you, end of the game -> reloads the game
 **************************************************/
+
+let user {
+
+}
+
+let aliens {
+
+}
+
+let galaxy {
+
+}
+
+let blackHole {
+
+}
+
+let nebula {
+
+}
+
+let pulsar {
+
+}
+
+let sun {
+
+}
+
+
+let userImg;
+let alienImg;
+let alienShipImg;
+let galaxyImg;
+let blackHoleImg;
+let nebulaImg;
+let pulsarImg;
+let sunImg;
+
+let engineStartSound;
+let engineOffSound;
+let gameThemeSound;
+let alienSound;
+
+function preload() {
+
+}
 
 // setup()
 //
